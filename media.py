8369d8c5a3f59496e25c252e97493e284d6b1932
mardymard <eymardlpz@gmail.com>
2017-07-19 20:06:31 -0700

    Added Docstring for methods

--- media.py
+++ media.py
@@ -1,11 +1,12 @@
 import webbrowser
 
-# A Movie class to create a movie object
+'''This is movie class will instantiate a movie object'''
 
 
 class Movie():
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
+    '''This constructor will initialize the movie object with given inputs'''
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
@@ -13,5 +14,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    '''This method will lauch a trailer for the movie that is selected'''
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
